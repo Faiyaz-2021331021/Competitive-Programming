a3a34367c93e71623a788dbb99042856b4468ec7
Faiyaz Ismail <137979191+Faiyaz-2021331021@users.noreply.github.com>
2024-09-24 12:21:29 +0600

    Update 01_Knapsack.cpp

--- 01_Knapsack.cpp
+++ 01_Knapsack.cpp
@@ -1,3 +1,5 @@
+///https://atcoder.jp/contests/abc060/tasks/arc073_b
+
 #include<bits/stdc++.h>
 using namespace std;
 #define int            long long
@@ -24,6 +26,24 @@ void edm()
 const int N = 1000;
 int wt[N];int val[N];
 int dp[N][N];
+map<pair<int,int>,int>mp;
+int knapsack(int n,int w) ///// IF weight/capacity has large value
+{
+    if(w<=0)return 0;
+    if(n<=0)return 0;
+    auto it = mp.find({n,w});
+    if(it!=mp.end())return mp[{n,w}];
+    if(wt[n-1]>w)
+    {
+        mp[{n,w}] = knapsack(n-1,w);
+    }
+    else
+    {
+        mp[{n,w}] = max((knapsack(n-1,w)),(knapsack(n-1,w-wt[n-1]) + val[n-1]));
+    }
+    return mp[{n,w}];
+}
+
 int knapsack(int n,int w)
 {
     if(w<=0)return 0;
